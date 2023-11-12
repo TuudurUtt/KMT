61cf88406b6db8e20d17b84659279f04c5cad3e3
TuudurUtt <tuudur.utt@gmail.com>
2023-11-12 18:30:46 +0200

    Navigatsiooniriba ja css avalehel

---
+++ Main.js
@@ -0,0 +1,8 @@
+function Navigate() {
+    var x = document.getElementById("Topnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
