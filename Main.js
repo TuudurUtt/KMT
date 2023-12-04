e3d833cba6c61f60a9d8ef94e85f43420a9f0d2c
TuudurUtt <tuudur.utt@gmail.com>
2023-12-04 22:58:01 +0200

    JS commentaarid

--- Main.js
+++ Main.js
@@ -1,7 +1,9 @@
+// JS funtsioon, mida kasutame lehtede vahel navigeerimiseks. Kood pärineb Tuudur Jürgen Utti eemlmisel aastal kirjutdatud projektist,
+// mis oli pea kindlasti sattunud sinna kusagilt Stack overflow sügavustest, kuid see allikas on kaotsi läinud.
 function Navigate() {
-    var x = document.getElementById("Topnav");
+    var x = document.getElementById("topnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className += "responsive";
     } else {
         x.className = "topnav";
     }
